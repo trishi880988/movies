e89304258ae9143a967e6f4e9a5f43f663ae7a29
trishi880988 <gauravyt8803@gmail.com>
2025-03-30 11:21:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8049101814:AAFdk0IVnd4WUaIc8YPo8PN0BfVmLRC
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6103594386').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/ytbr_67") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002587890850'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002666634838'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Rp1BJ_BKIHEzNzc1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002587890850').split()]
 #---------------------------------------------------------------
